2c6b17a4da93ff3f65482f878af12776c6b47d35
torumanabe <tooru0530eva@gmail.com>
2022-12-13 10:09:18 +0900

    ⚡️2nd args in useCallback useEffecnt

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -9,9 +9,11 @@ import { useCallback, useEffect, useState } from "react";
 export default function Home() {
   const [count, setCount] = useState(1)
 
-  const handleClick = (e) => {
-    setCount(count => count + 1)
-  };
+  const handleClick = useCallback((e) => {
+    if(count < 10){
+      setCount((count) => count + 1);
+    }
+  }, [count]);
 
   useEffect(() => {
     document.body.style.backgroundColor = "lightblue";
