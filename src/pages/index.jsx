56de20eb4f818ecce7c9ade79b91d02a9645998c
torumanabe <tooru0530eva@gmail.com>
2022-12-19 10:11:00 +0900

    ✨ ♻️ 状態変更時にprevをつける&useStateで配列を扱う

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -10,15 +10,16 @@ export default function Home() {
   const [count, setCount] = useState(1);
   const [ text, setText ] = useState("");
   const [ isShow, setIsShow ] = useState(true);
+  const [ array, setArray ] = useState([]);
 
   const handleClick = useCallback(() => {
     if(count < 10){
-      setCount((count) => count + 1);
+      setCount((prevCount) => prevCount + 1);
     }
   }, [count]);
 
   const handleDisplay = useCallback(() => {
-    setIsShow((isShow) => !isShow);
+    setIsShow((prevIsShow) => !prevIsShow);
   }, []);
 
   useEffect(() => {
@@ -36,6 +37,14 @@ export default function Home() {
     setText(e.target.value.trim());
   }, []);
 
+  const handleAdd = useCallback(() => {
+    setArray((prevArray) => {
+      if (prevArray.some(item => item === text)){
+        alert("同じ要素がすでに存在します。")
+      }
+      return [...prevArray, text];
+    })
+  }, [text]);
   return (
     <div className={styles.container}>
       <Head>
@@ -47,6 +56,14 @@ export default function Home() {
       <button onClick={handleClick}>ボタン</button>
       <button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
       <input type="text" value={text} onChange={handleChange} />
+      <button onClick={handleAdd}>追加</button>
+      <ul>
+        {array.map(item => {
+          return (
+            <li key={item}>{item}</li>
+          )
+        })}
+      </ul>
       <Main page="index" />
       <Footer/>
     </div>
